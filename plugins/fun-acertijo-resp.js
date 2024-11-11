bb364cd8923e54f0a93286d329556df953958f73
Inemagu <inemagu28@gmail.com>
2024-11-11 17:01:13 -0400

    Update fun-acertijo-resp.js

--- plugins/fun-acertijo-resp.js
+++ plugins/fun-acertijo-resp.js
@@ -1,22 +1,45 @@
 import similarity from 'similarity';
 const threshold = 0.72;
 const handler = (m) => m;
-handler.before = async function(m) {
+
+handler.before = async function(m, { conn }) {
   const id = m.chat;
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
-  this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('✨️ Ese acertijo ya ha terminado!');
-  if (m.quoted.id == this.tekateki[id][0].id) {
-    const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
-    if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
-      global.db.data.users[m.sender].estrellas += this.tekateki[id][2];
-      m.reply(`🌟 *Respuesta correcta!*\n+${this.tekateki[id][2]} Centavos`);
-      clearTimeout(this.tekateki[id][3]);
-      delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
-    else m.reply('Respuesta incorrecta!');
+
+  // Verifica si el mensaje citado es una respuesta válida a un acertijo enviado por el bot
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return true;
+
+  conn.tekateki = conn.tekateki || {};
+
+  // Verifica si existe un acertijo en curso para el chat actual
+  if (!(id in conn.tekateki)) {
+    m.reply('✨️ Ese acertijo ya ha terminado!');
+    return true;
   }
-  return !0;
+
+  // Obtiene los datos del acertijo
+  const acertijoData = conn.tekateki[id];
+
+  // Verifica si el ID del mensaje citado coincide con el ID del acertijo
+  if (m.quoted.id === acertijoData[0].id) {
+    const json = JSON.parse(JSON.stringify(acertijoData[1]));
+    const respuestaCorrecta = json.response.toLowerCase().trim();
+    const respuestaUsuario = m.text.toLowerCase().trim();
+
+    // Compara la respuesta del usuario con la respuesta correcta
+    if (respuestaUsuario === respuestaCorrecta) {
+      global.db.data.users[m.sender].estrellas += acertijoData[2];
+      m.reply(`🌟 *Respuesta correcta!*\n+${acertijoData[2]} Centavos`);
+      clearTimeout(acertijoData[3]);
+      delete conn.tekateki[id];
+    } else if (similarity(respuestaUsuario, respuestaCorrecta) >= threshold) {
+      m.reply('Casi lo logras!');
+    } else {
+      m.reply('Respuesta incorrecta!');
+    }
+  }
+
+  return true;
 };
+
 handler.exp = 0;
 export default handler;
