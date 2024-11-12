8f66d2174456d03f1c9467bb32eb0550b2af0ce7
Inemagu <inemagu28@gmail.com>
2024-11-12 12:07:53 -0400

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply('*`_☁️ Mi Velocidad es de:_`*' +  ` ${latensi.toFixed(4)} _ms_`);
+          m.reply('_*`☁️ Mi Velocidad es de:`*_' +  ` ${latensi.toFixed(4)} _ms_`);
             });
 }
 handler.help = ['ping']
