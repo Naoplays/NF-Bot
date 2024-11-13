0ea379c4f72e582f1546a47d6772a24bbdcdf324
Inemagu <inemagu28@gmail.com>
2024-11-13 06:24:17 -0400

    Update group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -16,7 +16,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     const isMedia = /image|video|sticker|audio/.test(mime);
     const more = String.fromCharCode(8206);
     const masss = more.repeat(850);
-    const htextos = `${text ? text : '*Chinguen a su madre*'}`;
+    const htextos = `${text ? text : '*Parale We Ni te Pelan*'}`;
     if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {image: mediax, mentions: users, caption: htextos, mentions: users}, {quoted: m});
@@ -30,7 +30,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m});
     } else {
-      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: image1, sourceUrl: 'https://whatsapp.com/channel/0029Vak6H9l3GJP4FJH5063R'}}}}}, {});
+      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: 'https://i.ibb.co/4Fyf88Z/file.jpg', sourceUrl: 'https://whatsapp.com/channel/0029Vak6H9l3GJP4FJH5063R'}}}}}, {});
     }
   }
 };
