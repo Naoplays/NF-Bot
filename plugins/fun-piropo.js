434bc9b44ff7009b114aec8c229304c257251ff1
Inemagu <inemagu28@gmail.com>
2024-11-20 06:15:11 -0400

    Update fun-piropo.js

--- plugins/fun-piropo.js
+++ plugins/fun-piropo.js
@@ -1,7 +1,7 @@
 /* By Sisked */
 
 const handler = async (m, {conn, text}) => {
-  m.reply(`*➢ "${pickRandom(global.piropo)}"*\n\n*𝑺𝑰𝑺𝑲𝑬𝑫 𝑩𝑶𝑻-𝑴𝑫 ⭐*`);
+  m.reply(`*➢ "${pickRandom(global.piropo)}"*\n\n𝙎𝙄𝙎𝙆𝙀𝘿 𝘽𝙊𝙏 ⭐`);
 };
 handler.help = ['piropo']
 handler.tags = ['fun'];
