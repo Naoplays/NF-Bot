0c6a5cf9c637e79fc7d8c6a27d045bc4da56e2a2
Inemagu <inemagu28@gmail.com>
2024-11-21 09:28:51 -0400

    Update group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -29,7 +29,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m});
     } else {
-      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: global.icono, sourceUrl: global.canal}}}}}, {});
+      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: 'https://telegra.ph/file/03d1e7fc24e1a72c60714.jpg', sourceUrl: global.canal}}}}}, {});
     }
   }
 };
