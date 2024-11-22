05506737b903fe0805782d1b419002235e3fb0d1
Inemagu <inemagu28@gmail.com>
2024-11-22 19:34:15 -0400

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -13,7 +13,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.reply(m.chat, `𝙀𝙣𝙡𝙖𝙘𝙚 𝙙𝙚𝙩𝙚𝙘𝙩𝙖𝙙𝙤 ⚠️\n\n𝘼𝙣𝙙𝙖 𝙖 𝙝𝙖𝙘𝙚𝙧 𝙩𝙪 𝙋𝙪𝙗𝙡𝙞𝙘𝙞𝙙𝙖𝙙 𝙖 𝙤𝙩𝙧𝙤 𝙡𝙖𝙙𝙤 *@${m.sender.split('@')[0]}* 𝙀𝙡𝙞𝙢𝙞𝙣𝙤 𝙩𝙪 𝙢𝙚𝙣𝙨𝙖𝙟𝙚 𝙮 𝙖 𝙩𝙞 𝙥𝙤𝙧 𝙚𝙨𝙘𝙤𝙧𝙞𝙖 ${isBotAdmin ? '' : '\n\n𝘿𝙚𝙗𝙤 𝙨𝙚𝙧 𝙖𝙙𝙢𝙞𝙣 𝙥𝙖𝙧𝙖 𝙚𝙭𝙥𝙪𝙡𝙨𝙖𝙧𝙡𝙤!!! :"v'}`, null, { mentions: [m.sender] } )
+        await conn.reply(m.chat, `*≡ Enlace Detectado*\n\nNo permitimos enlaces de otros grupos, lo siento *@${m.sender.split('@')[0]}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
